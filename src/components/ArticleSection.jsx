b2684adf4f1f7f1d2e669cc856ef875670adcdc5
Kong Thiraphat <thiraphat.cto@gmail.com>
2024-10-27 12:19:40 +0700

    feat: add fetchdata from api

--- src/components/ArticleSection.jsx
+++ src/components/ArticleSection.jsx
@@ -4,7 +4,8 @@ import * as React from "react";
 import { blogPosts } from "./data/blogPost";
 import authorImage from "../assets/author-image.jpg";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import {
   Select,
@@ -19,86 +20,109 @@ import {
 function ArticleSection() {
   const categories = ["Highlight", "Cat", "Inspiration", "General"];
   const [category, setCategory] = useState("");
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://blog-post-project-api.vercel.app/posts`
+      );
+      setPost(response.data.posts);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div
-      className="bg-[#F9F8F6] md:px-32"
-      style={{ fontFamily: "Poppins, sans-serif" }}
-    >
-      <h2 className="text-2xl text-[#26231E] font-semibold mb-4 px-4">
-        Latest articles
-      </h2>
-      <div className="bg-[#EFEEEB] p-4 rounded-md h-44 justify-center items-center md:rounded-2xl md:h-28">
-        <div className="md:flex md:justify-between md:items-center h-16">
-          <div className="hidden md:flex gap-2 font-medium m-3 justify-center items-center">
-            <Tabs>
-              <TabsList className="bg-[#EFEEEB] gap-10">
-                {categories.map((tabs) => (
-                  <TabsTrigger
-                    key={tabs}
-                    value={tabs}
-                    className={`text-[#75716B] text-base py-3 px-4 rounded-xl
+    <>
+      <div
+        className="bg-[#F9F8F6] md:px-32"
+        style={{ fontFamily: "Poppins, sans-serif" }}
+      >
+        <h2 className="text-2xl text-[#26231E] font-semibold mb-4 px-4">
+          Latest articles
+        </h2>
+        <div className="bg-[#EFEEEB] p-4 rounded-md h-44 justify-center items-center md:rounded-2xl md:h-28">
+          <div className="md:flex md:justify-between md:items-center h-16">
+            <div className="hidden md:flex gap-2 font-medium m-3 justify-center items-center">
+              <Tabs>
+                <TabsList className="bg-[#EFEEEB] gap-10">
+                  {categories.map((tabs) => (
+                    <TabsTrigger
+                      key={tabs}
+                      value={tabs}
+                      className={`text-[#75716B] text-base py-3 px-4 rounded-xl
                       ${
                         category === tabs
                           ? "bg-[#DAD6D1] text-black"
                           : "hover:bg-[#F9F6F8]"
                       }`}
-                    onClick={() => setCategory(tabs)}
-                  >
-                    {tabs}
-                  </TabsTrigger>
-                ))}
-              </TabsList>
-            </Tabs>
+                      onClick={() => setCategory(tabs)}
+                    >
+                      {tabs}
+                    </TabsTrigger>
+                  ))}
+                </TabsList>
+              </Tabs>
+            </div>
+            <div className="relative md:mt-6 md:flex">
+              <Input
+                type="text"
+                placeholder="Search"
+                className="w-80 mb-4 gap-4"
+              />
+              <Search
+                className="absolute right-5 top-1/2 transform -translate-y-1/2 text-gray-500 md:right-3 md:top-5"
+                size={18}
+              />
+            </div>
           </div>
-          <div className="relative md:mt-6 md:flex">
-            <Input
-              type="text"
-              placeholder="Search"
-              className="w-80 mb-4 gap-4"
-            />
-            <Search
-              className="absolute right-5 top-1/2 transform -translate-y-1/2 text-gray-500 md:right-3 md:top-5"
-              size={18}
-            />
+          <div className="md:hidden w-full">
+            <h6 className="px-1 text-[#75716B] font-medium">Category</h6>
+            <Select
+              value={category}
+              onValueChange={(value) => setCategory(value)}
+            >
+              <SelectTrigger className="w-80">
+                <SelectValue placeholder="Highlight" />
+              </SelectTrigger>
+              <SelectContent>
+                {categories.map((cat) => {
+                  return (
+                    <SelectItem key={cat} value={cat}>
+                      {cat}
+                    </SelectItem>
+                  );
+                })}
+              </SelectContent>
+            </Select>
           </div>
         </div>
-        <div className="md:hidden w-full">
-          <h6 className="px-1 text-[#75716B] font-medium">Category</h6>
-          <Select
-            value={category}
-            onValueChange={(value) => setCategory(value)}
-          >
-            <SelectTrigger className="w-80">
-              <SelectValue placeholder="Highlight" />
-            </SelectTrigger>
-            <SelectContent>
-              {categories.map((cat) => {
-                return (
-                  <SelectItem key={cat} value={cat}>
-                    {cat}
-                  </SelectItem>
-                );
-              })}
-            </SelectContent>
-          </Select>
-        </div>
+        <article className="grid grid-cols-1 px-4 !md:px-0 mt-8 md:grid-cols-2 gap-8 ">
+          {post.map((blog, index) => {
+            return (
+              <BlogCard
+                key={index}
+                image={blog.image}
+                category={blog.category}
+                title={blog.category}
+                description={blog.description}
+                author={blog.author}
+                date={new Date(blog.date).toLocaleDateString("en-GB", {
+                  day: "numeric",
+                  month: "long",
+                  year: "numeric",
+                })}
+              />
+            );
+          })}
+        </article>
       </div>
-      <article className="grid grid-cols-1 px-4 md:grid-cols-2 gap-8 md:px-0 mt-8">
-        {blogPosts.map((blog, index) => {
-          return (
-            <BlogCard
-              key={index}
-              image={blog.image}
-              category={blog.category}
-              title={blog.category}
-              description={blog.description}
-              author={blog.author}
-              date={blog.date}
-            />
-          );
-        })}
-      </article>
-    </div>
+    </>
   );
 }
 
